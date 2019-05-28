40542ed57e2aa1d501aaa10f9484e77249a76a21
unknown <tansenananda@gmail.com>
2019-05-29 01:24:01 +0600

    Task3

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function sum(a){
 
 }
 console.log(sum([1, 2, 3, 4, 5]))
+
+
+//Task 3
+
+function valEmail(email){
+
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.exec(email);
+ }
+ 
+ var check = valEmail('1510505@iub.edu.bd');
+ 
+ console.log(check);
+
+ var c = valEmail('tansenananda@gmail.com');
+
+ console.log(c);
+ 
+ console.log(valEmail('kmllnm.com'));
+ 
