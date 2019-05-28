50dc85b3ef97150e3af2550013edc76544db99a2
unknown <tansenananda@gmail.com>
2019-05-29 01:01:11 +0600

    Task2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /* JavaScript code here */
+console.log("External Javascript")
+
+
+//Task 2:
+
+function sum(a){
+
+    var sum = 0;
+
+    a.forEach(function(i, index){
+
+        sum = sum + i;
+
+    });
+
+    return sum;
+
+}
