d1877fee68d79a5c6be5077345a59ffdd83357ed
cis101 <tansenananda@gmail.com>
2019-05-29 08:50:29 +0600

    Task4

--- script.js
+++ script.js
@@ -37,3 +37,56 @@ function valEmail(email){
  
  console.log(valEmail('kmllnm.com'));
  
+ 
+ // Objects Example
+
+ var company = {
+    name: 'Google', 
+    age: 19, 
+    ceo: {
+    first_name: 'Sundar', 
+    last_name: 'Pichai'
+    }
+    
+};
+
+console.log(company)
+
+company.updateAge = function(newAge){ 
+    this.age = newAge }
+
+company.updateAge(23);
+
+console.log(company)
+
+
+//Task 4: Access object property at a scpecial index of array
+
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+    
+    function getProp(index, prop){
+        return library[index][prop]
+    }
+
+    console.log(getProp(1, 'author'))
+
+    function getReadingstatus(index){
+        return library[index].readingStatus;
+    }
+
+    console.log(getReadingstatus(1))
